9975252aae9715f515d783369be27489f0857057
ravimoolimani <132146951+ravimoolimani@users.noreply.github.com>
2025-08-30 14:47:10 +0530

    Add files via upload

---
+++ frontend/src/components/footer/Footer.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import "./footer.css";
+import {
+  AiFillFacebook,
+  AiFillTwitterSquare,
+  AiFillInstagram,
+} from "react-icons/ai";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="footer-content">
+        <p>
+          &copy; Cracko MockTest. All rights reserved. <br /> Made
+          By ❤️ <a href="">M.S.Ravi</a>
+        </p>
+        <div className="social-links">
+          <a href="">
+            <AiFillFacebook />
+          </a>
+          <a href="">
+            <AiFillTwitterSquare />
+          </a>
+          <a href="">
+            <AiFillInstagram />
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
